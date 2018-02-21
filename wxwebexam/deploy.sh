c3db0353b18db8b41189fb1b157913b512131b8b
JackyShen <mebusw@163.com>
2018-02-22 00:53:33 +0800

    update deploy script

--- wxwebexam/deploy.sh
+++ wxwebexam/deploy.sh
@@ -2,5 +2,5 @@ ps aux|grep gunicorn |awk '{print $2}'|xargs kill -9
 python manage.py dumpdata  --format json   > /data/db-backup/`date +"%Y-%m-%d-%H-%M-%S"`.json
 ###cp db.sqlite3 db.sqlite3~
 git pull
-###python manage.py loadata fixture.json
+###python manage.py loaddata --app scrum.entry fixture.json
 gunicorn --workers=2 -k gevent wxwebexam.wsgi:application &
